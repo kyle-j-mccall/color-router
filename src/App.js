9e81772dd82380d4459a9584b6200f93c77a7bd3
Kyle McCall <kylem147@gmail.com>
2023-01-09 17:24:12 -0800

    react 6 migration

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-
 import { Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/Header';
 import Main from './components/main/Main';
-import { Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import NotFound from './components/notFound/NotFound';
-
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Switch>
-        <Route path='/rgb/:red/:green/:blue' component={Main}/>
-        <Route path='*' component={NotFound}/>
-      </Switch>
-      
+      <Header />
+      <Routes>
+        <Route path="/rgb/:red/:green/:blue" element={<Main />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
